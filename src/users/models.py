55efa05810d5012a24ba61ef0d5bdb09d22a2e01
Maxim <makspyatkin2107@gmail.com>
2025-10-11 14:25:18 +0300

    update

--- src/users/models.py
+++ src/users/models.py
@@ -2,13 +2,16 @@ from sqlalchemy import Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
+from templates.sql_enums import GenderEnum
 
 
 class Users(Base):
     __tablename__ = "users"
 
-    id = Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    tg_id = Mapped[int]
-    name = Mapped[str]
-    age = Mapped[int]
-    about_user = Mapped[str]
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    tg_id: Mapped[int]
+    user_gender: Mapped[GenderEnum]
+    name: Mapped[str]
+    age: Mapped[int]
+    city: Mapped[str]
+    interests: Mapped[str | None]
