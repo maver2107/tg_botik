547817ca962832398a507f65178030d9134865dd
Maxim <makspyatkin2107@gmail.com>
2025-10-20 14:19:23 +0300

    миграция в бд и мелкие исправления

--- migration/env.py
+++ migration/env.py
@@ -6,8 +6,8 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from src.bot.models.user import Users  # noqa: F401
 from src.core.database import DATABASE_URL, Base
-from src.users.models import Users  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- src/bot/enum/gender.py
+++ src/bot/enum/gender.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class Gender(Enum):
+class Gender(str, Enum):
     MALE = "male"
     FEMALE = "female"
     SKIP_GENDER = "not important"
